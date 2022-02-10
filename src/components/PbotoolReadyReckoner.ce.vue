b6677501bd29d534596e9b3b13dd1eb53a7672a0
root <remy.vanherweghem@parl.gc.ca>
2022-02-10 14:33:53 +0000

    Removed some boilerplate code.

---
+++ src/components/PbotoolReadyReckoner.ce.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { ref } from 'vue'
+import RrHeader from "./RrHeader/RrHeader.vue"
+</script>
+
+<template>
+<rr-header></rr-header>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { defineCustomElement } from 'vue'
 
-createApp(App).mount('#app')
+import PbotoolReadyReckoner from "./components/PbotoolReadyReckoner.ce.vue"
+customElements.define('pbotool-rrsb', defineCustomElement(PbotoolReadyReckoner))
