cc6d66a6a99db42811deb213390c6d1828401a02
root <remy.vanherweghem@parl.gc.ca>
2022-02-11 18:58:16 +0000

    Made a few adjustments to allow app to work on ABCMS.

--- src/App.ce.vue
+++ src/App.ce.vue
@@ -2,28 +2,32 @@
   <Intro></Intro>
   <Meta v-if="payload"></Meta>
   <div v-if="payload">Loaded</div>
+
   <Outro></Outro>
 </template>
 
-<script setup>
+<script>
+import Strings from "./Strings.js";
+import payloadUrl from "./assets/payload.json?url";
 import Intro from "./components/Layout/Intro.vue";
 import Meta from "./components/Layout/Meta.vue";
 import Outro from "./components/Layout/Outro.vue";
-import { ref } from "vue";
-import payloadUrl from "./assets/payload.json?url";
-import Strings from "./Strings.js";
 
 const language = document.documentElement.lang;
-const strings = new Strings(language);
-let payload = ref(null);
-defineExpose({
-  strings,
-  language,
-  payload,
-});
-</script>
-<script>
+
 export default {
+  data() {
+    return {
+      language: language,
+      strings: new Strings(language),
+      payload: null,
+    };
+  },
+  components: {
+    Intro: Intro,
+    Meta: Meta,
+    Outro: Outro,
+  },
   mounted() {
     fetch(payloadUrl)
       .then((r) => r.json())
